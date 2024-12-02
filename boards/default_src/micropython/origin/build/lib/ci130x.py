4b030065b0ea80f7a7d557750a3f52c572c29637
dahanzimin <353767514@qq.com>
2024-12-02 11:17:03 +0800

    更新雷达和语音识别库问题

--- boards/default_src/micropython/origin/build/lib/ci130x.py
+++ boards/default_src/micropython/origin/build/lib/ci130x.py
@@ -1,7 +1,7 @@
 """
-CI230X
+CI130X
 
-MicroPython library for the CI230X (ASR-I2C)
+MicroPython library for the CI130X (ASR-I2C)
 =======================================================
 @dahanzimin From the Mixly Team
 """
@@ -16,7 +16,7 @@ _CI_ID_NUM			= const(0x06)
 _CI_ID_CLE			= const(0x07)
 _CI_ID_END			= const(0x5A)
 
-class CI230X:
+class CI130X:
 	def __init__(self, i2c_bus, addr=_CI_ADDRESS):
 		self._device  = i2c_bus
 		self._address = addr
@@ -24,7 +24,7 @@ class CI230X:
 		try:
 			self._rreg(_CI_ID_GET, 3)
 		except:
-			raise AttributeError("Cannot find a CI230X")
+			raise AttributeError("Cannot find a CI130X")
 
 	def _wreg(self, reg):
 		'''Write memory address'''
@@ -44,6 +44,7 @@ class CI230X:
 		_buf = self._rreg(_CI_ID_GET, 3)
 		if not repeat:
 			self._wreg(bytes([_CI_ID_CLE, 0, 0, _CI_ID_END]))
+			time.sleep_ms(50)
 		self._cmd_id = _buf[0] if _buf[2] == _CI_ID_END else None
 		return self._cmd_id
 
