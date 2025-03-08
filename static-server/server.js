4b12cb4662a8a386960466657c4534a9c4542e85
王立帮 <3294713004@qq.com>
2025-03-08 12:40:59 +0800

    Update: static-server中增加移除nwjs cache功能

--- static-server/server.js
+++ static-server/server.js
@@ -1,5 +1,17 @@
+const fs = require('fs').promises
 const StaticServer = require('./static-server.js');
 const SSLStaticServer = require('./static-sslserver.js');
+
+async function deleteDirectory(dirPath) {
+    try {
+        await fs.rm(dirPath, { recursive: true, force: true });
+        console.log('Directory deleted successfully.');
+    } catch (err) {
+        console.error('Error deleting directory:', err);
+    }
+}
+
+deleteDirectory('./nw_cache');
 
 const init = () => {
     StaticServer.run('7000');
